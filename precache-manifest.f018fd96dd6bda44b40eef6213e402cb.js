05f7aa943b6c459ecb5ccd6e8c6529505a24a6dd
Dakota <dakota.talley@gmail.com>
2020-11-14 11:01:04 -0500

    Updates

---
+++ precache-manifest.f018fd96dd6bda44b40eef6213e402cb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af08e3c61f32c476faa690fbb5d2dca5",
+    "url": "/yc-react/index.html"
+  },
+  {
+    "revision": "10a15c8d20d5572866f7",
+    "url": "/yc-react/static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "5387a036ae5957191aea",
+    "url": "/yc-react/static/js/2.543b0d73.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/yc-react/static/js/2.543b0d73.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "10a15c8d20d5572866f7",
+    "url": "/yc-react/static/js/main.ccfe8d30.chunk.js"
+  },
+  {
+    "revision": "62650246132a27cd9a25",
+    "url": "/yc-react/static/js/runtime-main.8430bc43.js"
+  }
+]);
